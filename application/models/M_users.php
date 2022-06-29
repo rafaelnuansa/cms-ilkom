cb4a5c4534141d7082c649c6f25ef3cb5f62438e
srimeiylanirejeki29 <93021732+srimeiylanirejeki29@users.noreply.github.com>
2022-06-29 22:27:06 +0700

    Update M_users.php | Sri meiylani rejeki

--- application/models/M_users.php
+++ application/models/M_users.php
@@ -1,16 +1,13 @@
 <?php
 class M_users extends CI_Model{
-
 	function get_all_users(){
 		$query=$this->db->query("SELECT * FROM users ORDER BY id DESC");
 		return $query;	
 	}
-
 	function saveUser($username,$nama_lengkap,$password){
 		$query=$this->db->query("INSERT INTO users (username,nama_lengkap,password) VALUES ('$username','$nama_lengkap',md5('$password'))");
 		return $query;
 	}
-
 	function update($id,$username,$nama_lengkap,$password){
 		$query=$this->db->query("UPDATE users set username='$username',nama_lengkap='$nama_lengkap',password=md5('$password') where id='$id'");
 		return $query;
@@ -37,9 +34,11 @@ class M_users extends CI_Model{
 	}
 
 	function get_user_login($id){
-		$query=$this->db->query("SELECT * FROM users where id='$id'");
+		$query=$this->db->query 
+			(
+			"SELECT * 
+		FROM users 
+		where id='$id'");
 		return $query;
 	}
-
-
 }
