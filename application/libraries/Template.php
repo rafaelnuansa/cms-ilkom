257b84c1f7ebfac3c9efe17eefe783adb661a2c6
srimeiylanirejeki29 <93021732+srimeiylanirejeki29@users.noreply.github.com>
2022-06-29 22:20:26 +0700

    Update Template.php | Sri meiylani rejeki

--- application/libraries/Template.php
+++ application/libraries/Template.php
@@ -1,21 +1,15 @@
 <?php
-
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 // Library by Rafael Nuansa 
 class Template {
-
-    protected $_CI;
-
+protected $_CI;
     function __construct() {
         $this->_CI = &get_instance();
     }
-
     function display($template, $data = null) {
-        $data['_content'] = $this->_CI->load->view($template, $data, true);
+        $data['_content']=$this->_CI->load->view($template, $data, true);
+		
         $this->_CI->load->view('template/master', $data);
     }
-
 }
-
-
